3bb4cd0bc4ebef1b26a187db584b4fb31544629e
kalanoka <115204665+kalanoka@users.noreply.github.com>
2022-11-03 21:20:19 -0700

    Update BaseCalc_Interface.py

--- Interfaces/BaseCalc_Interface.py
+++ Interfaces/BaseCalc_Interface.py
@@ -1,6 +1,5 @@
 # Importation des modules utiles
 import tkinter as tk
-VALUES = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 #Dictionnaire des options pour les menus
 OPTIONS = {
@@ -21,6 +20,14 @@ OPTIONS = {
     "Hexadecimal (16)" : 16
 }
 
+#Characteres qui peuvent etres saisis par l'utilisateur
+CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ+-/*%().'
+
+#All digits, characters used to represent values
+VALUES = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+OPERATIONS = '+-/*%().'
+
+# Définition des fonctions
 def numberToBase(num,fromB = 10,toB = 10):
     '''Converts a number inputed as a string from one base to another
     Arguments:
@@ -51,98 +58,124 @@ def numberToBase(num,fromB = 10,toB = 10):
           num = abs(num)
           negative = True
       while num > 0:
-          result = VALUES[num%toB].upper() + result
+          result = VALUES[num%toB] + result
           num //= toB
       if negative:
           result = '-' + result
-    
     return result
 
-def convertBase() :
-    """Affiche la conversion de la base
-
+def stringToBase(saisie, convert_from = 10, convert_to = 10):
+    '''Converts all the numbers in an operation inputed as a string from one base to another
     Arguments:
-    None -- (fonction déclenchée par le bouton - pas de saisie)
+    > saisie -- string
+    > convert_from, base to convert from -- int [2,36]
+    > convert_to, base to convert to -- int [2,36]
+    returns:
+    - string
+    '''
+    conv_saisie=''
+    temp = ''
+    for i in range(len(saisie)):
+        if saisie[i] not in OPERATIONS:
+            temp += saisie[i]
+        else:
+            if temp.isalnum():
+                conv_saisie += numberToBase(temp,convert_from,convert_to)
+            conv_saisie += saisie[i]
+            temp = ''
+    if temp:
+        conv_saisie += numberToBase(temp,convert_from,convert_to)
+    return conv_saisie
+
+def baseEval_str(saisie,convert_from,convert_to):
+    ''' Takes an oppeartion formated as a string and calculates the result as a string
     
-    Return :
-    None -- (remplissage d'une zone de texte)
-    """
+    Arguments:
+    > saisie -- str (operation in original base)
+    > convert_from -- int (base to convert from)
+    > convert_to -- int (base to convert to)
     
-    #Gets entry from user
-    saisie = champSaisie.get()
-    
-    #Gets the two bases for conversion from user
-    convert_from = OPTIONS.get(clicked.get())
-    convert_to = OPTIONS.get(clicked2.get())
-    
-    #Changes text in the base fields
-    base1_label.config(text=convert_from)
-    base2_label.config(text=convert_to)
-    
-    #Checks if number entered is float
-    x = saisie.find('.')
-    if x >= 0:
-            saisie += '0'
-            saisie = saisie[:x+3]
-            saisie2 = saisie[x+1:]
-            saisie = saisie[:x]
+    returns:
+    - string (result of operation in wanted base)
+    '''
+    try:
+        #First, we parse saisie and we convert the numbers to decimal
+        conv_saisie = stringToBase(saisie,convert_from)
+        print('conv_saisie:',conv_saisie)
+        #Calculate operation in decimal, put into 'resultat'
+        resultat = str(eval(conv_saisie))
+        
+        #Checks if resultat is a float and rounds, then separates into 2
+        x = resultat.find('.')
+        if x >= 0:
+            resultat += '0'
+            resultat = resultat[:x+3]
+            resultat2 = resultat[x+1:]
+            resultat = resultat[:x]
             
             #Calculate decimal part in wanted base
             #Define variables
-            if convert_to <= 3:
-                i=0
-                res = ''
-                temp = 0
-                num = float(saisie2)/100
-                #Calculation loop
-                while (i < 10) and round(temp,2)!=1.00:
-                    temp = float(num*int(convert_to))
-                    strTemp = str(temp)
-                    res += strTemp[0]
-                    if temp > 1:
-                        temp -= 1
-                    num = temp
-                    i += 1
-            else:
-                res = str(((int(saisie2)/100)*int(convert_to))*100)
-                res = res[:res.find('.')]
-                while res[-1] == '0':
-                    res = res[:-1]
+            i=0
+            res = ''
+            temp = 0.1
+            num = float(resultat2)/100
+            #Calculation loop
+            while (i < 10) and round(temp,2)!=int(temp):
+                temp = float(num*int(convert_to))
+                strTemp = str(temp)
+                res += strTemp[0]
+                if temp > (convert_to-1):
+                    temp -= (convert_to-1)
+                num = temp
+                i += 1
+                print(temp,round(temp,2))
+        
+        #convert result to wanted base
+        if x >= 0:
+            resultat = numberToBase(resultat,10,convert_to)+'.'+res
+        else:
+            resultat = numberToBase(resultat,10,convert_to)
+        
+        print(resultat)
+        return(resultat)
+    
+    except Exception as e:
+        print(e)
+        raise
+
+def calculate() :
+    
+    saisie = champSaisie.get()
+         
+    convert_from = OPTIONS.get(clicked.get())
+    convert_to = OPTIONS.get(clicked2.get())
     
     try:
-        #Checks the entry is a valid number that can be converted
-        if x >= 0:
-            result = numberToBase(saisie,convert_from,convert_to)+'.'+res
-        else:
-            result = numberToBase(saisie,convert_from,convert_to)
+        #Check that inputed operation is valid
+        for i in range(len(saisie)):
+            assert CHARS.find(saisie[i].upper()) > -1
+        #calculate result of operation
+        calc_result = baseEval_str(saisie,convert_from,convert_to)
+    except Exception as e:
+        print(e)
+        result_data.config(text='Enter valid operation')
         
-        #Changes label box to show result
-        result_data.config(text=result)
-    except:
-        result_data.config(text='Enter valid number')
-    
-    return
-
+    else:
+        result_data.config(text=calc_result)
+        
 def aide() :
     
-    aide_txt = "Convertisseur de Base\n\nFonction\n\nCe programme permet à l'utilisateur d'entrer un nombre dans\nn'importe quelle base (de 2 à 16) et de le convertir dans une autre base.\n\nUtilisation\n\nL'utilisateur doit d'abord choisir la base dans laquelle se trouve le\nnombre saisi et la base vers laquelle il veut convertir le nombre.\nEnsuite, l'utilisateur doit saisir le nombre qu'il souhaite convertir et\n appuyer sur le bouton « Convertir » pour effectuer le calcul.\n\nRemarques\n\nLa zone de saisie ne fonctionne qu'avec les caractères possible.\ndans la base choisie, et renvoie une erreur lorsque ce n'est pas le cas.\n"
-    
-    popup = tk.Toplevel(root)
-    popup.geometry('450x320')
-    popup.title('Aide Calculator')
-    popup.configure(background='#e4e4e4')
-    aide_label = tk.Label(popup, text=aide_txt,padx=50,pady=25,bg='#e4e4e4')
-    aide_label.pack()
+    print('aide')
         
     return
-
-# Création de la fenêtre tkinter
+    
 def create_window():
-  global result_data, champSaisie, clicked, clicked2, base1_label, base2_label, root, fenetre
+  global root, result_data, champSaisie, clicked, clicked2
   
+  # Création de la fenêtre tkinter
   root = tk.Tk()
-  root.geometry('400x250')
-  root.title('Convertisseur de base')
+  root.geometry('320x190')
+  root.title('Base Calculator')
   root.configure(background='#e4e4e4')
   
   # Création d'une autre frame pour la centrer
@@ -150,67 +183,58 @@ def create_window():
   fenetre.pack()
   fenetre.configure(background='#e4e4e4')
   
-  #CREATION DES BOUTONS
-  bouton_quitter = tk.Button(fenetre, text='Quitter', command=root.destroy)
-  bouton_quitter.grid(row=6, column=0, padx=6, pady=6, ipadx=5)
+  # Création des boutons 
+  bouton_lancer = tk.Button(fenetre, text='Calculer', command=calculate)
+  bouton_lancer.grid(row=4, column=0, padx=6, pady=6, ipadx=5)
   
-  bouton_convertir = tk.Button(fenetre, text='Convertir', command=convertBase)
-  bouton_convertir.grid(row=4, column=1, padx=6, pady=6, ipadx=5)
+  bouton_quitter = tk.Button(fenetre, text='Quitter', command=quitter)
+  bouton_quitter.grid(row=4, column=1, padx=6, pady=6, ipadx=5)
   
   bouton_aide = tk.Button(fenetre,text='?',command=aide)
-  bouton_aide.grid(row=0,column=4,sticky='E')
+  bouton_aide.grid(row=0,column=1,sticky='E')
   
-  #CREATIONS DES ZONES DE TEXTE
-  entete = tk.Label(fenetre, text='       Convertisseur de Base       ', font=('Arial', 14, 'bold'), fg='#0c6bab', bg='#e4e4e4')
-  entete.grid(row=0, column=0, columnspan=4,pady=10)
+  # Création des zones de texte 
+  entete = tk.Label(fenetre, text='Base Calculator', font=('Arial', 14, 'bold'), fg='#0c6bab', bg='#e4e4e4')
+  entete.grid(row=0, column=0, columnspan=2, pady=10)
   
-  from_label = tk.Label(fenetre, text='From', bg='#e4e4e4')
-  from_label.grid(row=1, column=0)
+  calcul_label = tk.Label(fenetre, text='Operation', pady=5, bg='#e4e4e4')
+  calcul_label.grid(row=1, column=0)
   
-  to_label = tk.Label(fenetre, text='To', bg='#e4e4e4')
-  to_label.grid(row=2, column=0)
+  result_label = tk.Label(fenetre, text='Resultat', pady=5, bg='#e4e4e4')
+  result_label.grid(row=2, column=0)
   
-  enter_label = tk.Label(fenetre, text='Saisis', bg='#e4e4e4')
-  enter_label.grid(row=3, column=0)
+  result_data = tk.Label(fenetre, text='',width=15, font=('Arial', 11), bg="#fff") 
+  result_data.grid(row=2, column=1)
   
-  result_label = tk.Label(fenetre, text='saisie', bg='#e4e4e4')
-  result_label.grid(row=5, column=0)
-  
-  base1_label = tk.Label(fenetre, text='N/A', bg='#e4e4e4')
-  base1_label.grid(row=3, column=2)
-  
-  base2_label = tk.Label(fenetre, text='N/A', bg='#e4e4e4')
-  base2_label.grid(row=5, column=2)
-  
-  result_data = tk.Label(fenetre, text='',width=15, font=('Arial', 11), bg="#fff")  # par exemple
-  result_data.grid(row=5, column=1)
-  
-  #CREATION DES CHAMPS DE SAISIE
+  # Création du champ de saisie
   champSaisie = tk.Entry(fenetre, font='12', width=15)
-  champSaisie.grid(row=3, column=1)
+  champSaisie.grid(row=1, column=1)
   
-  #CREATION DES MENUS
-  #clicked is variable for choice of base form user
+  # Création des menus
   clicked = tk.StringVar(fenetre)
-  clicked.set( "Choose base" )
-  #creation of menu using options from the keys of options dictionary
+  clicked.set( "Decimal (10)" )
   base1_menu = tk.OptionMenu(fenetre, clicked, *OPTIONS.keys())
-  base1_menu.grid(row=1, column=1)
+  base1_menu.grid(row=3, column=0)
   
-  #clicked is variable for choice of base form user
   clicked2 = tk.StringVar(fenetre)
-  clicked2.set( "Choose base" )
-  #creation of menu using options from the keys of options dictionary
+  clicked2.set( "Decimal (10)" )
   base2_menu = tk.OptionMenu(fenetre, clicked2, *OPTIONS.keys())
-  base2_menu.grid(row=2, column=1)
+  base2_menu.grid(row=3, column=1)
   
   # Programme principal 
   fenetre.mainloop()    # Boucle d'attente des événements
 
-
-
+def quitter():
+    global root
+    try:
+        root.destroy()
+    except:
+        pass
 
 def main():
-  create_window()
+    quitter()
+    create_window()
+
 if __name__ == "__main__":
     main()
+
