9c4fca7ac9b6365384ec4f428e318a4c20d0b3af
LobsterZerg <115204696+LobsterZerg@users.noreply.github.com>
2022-11-08 21:46:00 -0800

    Update BaseCalc_Interface.py

--- Interfaces/BaseCalc_Interface.py
+++ Interfaces/BaseCalc_Interface.py
@@ -192,8 +192,32 @@ def baseEval_str(saisie,convert_from,convert_to):
         print(e)
         raise
 
+def octets(bit_s):
+    '''Complete with zeros to multiple of 4 bits'''
+    if len(bit_s)%4 != 0:
+        bit_s = ''.join('0' for i in range(4-len(bit_s)%4)) + bit_s
+    return bit_s
+ 
+def comp2(bit_s):
+    '''Converts a binary number into a negative complement à deux number'''
+    bit_s = str(bit_s)
+    result = str(bit_s)
+    if int(bit_s) != 0:
+        bit_s = octets(bit_s)
+        inverse_s = ''.join(['1' if i == '0' else '0' for i in bit_s])
+        dec = int(inverse_s,2)+1
+        result = bin(dec)[2:]
+    result = octets(result)
+    result = ' '.join(result[i:i+4] for i in range(0, len(result), 4))
+    return result
+
+def complement():
+    complement = messagebox.askquestion("Complément à deux",
+                            "Votre resultat est un nombre entier négatif binaire.\nVoulez-vous le convertir en complément à deux ?",
+                           icon = 'question')
+    return complement
+
 def calculate() :
-    
     saisie = champSaisie.get()
          
     convert_from = OPTIONS.get(clicked.get())
@@ -218,30 +242,6 @@ def calculate() :
         
         #Affiche resultat
         result_data.config(text=calc_result)
-        
-def octets(bit_s):
-    if len(bit_s)%4 != 0:
-        bit_s = ''.join('0' for i in range(4-len(bit_s)%4)) + bit_s
-    
-    return bit_s
- 
-def comp2(bit_s):
-    bit_s = str(bit_s)
-    result = str(bit_s)
-    if int(bit_s) != 0:
-        bit_s = octets(bit_s)
-        inverse_s = ''.join(['1' if i == '0' else '0' for i in bit_s])
-        dec = int(inverse_s,2)+1
-        result = bin(dec)[2:]
-    result = octets(result)
-    result = ' '.join(result[i:i+4] for i in range(0, len(result), 4))
-    return result
-
-def complement():
-    complement = messagebox.askquestion("Complément à deux",
-                            "Votre resultat est un nombre entier négatif binaire.\nVoulez-vous le convertir en complément à deux ?",
-                           icon = 'question')
-    return complement
     
 def aide() :
     
